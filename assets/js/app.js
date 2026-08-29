a6c8edab34908c8bf00aa73f4223d7038432e613
Nicolás Riquelme <nicol.riquelmec@duocuc.cl>
2026-08-29 04:39:01 -0400

    Se agregó la función para que el icono solo apareciera en dispositivos moviles o pantallas pequeñas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,36 @@
 // EN ESTE JAVASCRIPT ESTARÁN LOS SCRIPST PARA LA PAGINA
 
-{fetch('header.html')
+{fetch('header.html') // ESTE ES PARA HACER EL HEADER PLANTILLA
     .then(response => response.text())
     .then(data => document.getElementById('header-contenedor').innerHTML = data);
 
 }
 
-{fetch('footer.html')
+{fetch('footer.html') // ESTE ES PARA HACER EL FOOTER PLANTILLA
     .then(response => response.text())
     .then(data => document.getElementById('footer-contenedor').innerHTML = data);
 }
+
+
+// ESTE ES LA FUNCION PARA QUE EL ICONO DE INICIAR SESIÓN SOLO APAREZCA PARA DISPOSIVOS MOVILES
+const mediaQuery = window.matchMedia('(min-width: 1280px)');
+
+function aparecerIcono(e) {
+    const iconoInicio = document.querySelector('.icono-iniciar');
+    
+    if (!iconoInicio) return; 
+
+    if (e.matches) {
+        iconoInicio.style.display = 'none';
+    } else {
+        iconoInicio.style.display = 'block';
+    }
+}
+
+aparecerIcono(mediaQuery);
+
+if (mediaQuery.addEventListener) {
+    mediaQuery.addEventListener('change', aparecerIcono);
+} else {
+    mediaQuery.addEventListener(aparecerIcono);
+} // ESTE ES LA FUNCION PARA QUE EL ICONO DE INICIAR SESIÓN SOLO APAREZCA PARA DISPOSIVOS MOVILES
